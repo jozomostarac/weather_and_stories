5292448eb4a66e463230b38a4a296f4d5e7a9e20
Jozo Mostarac <jozomostarac@gmail.com>
2024-10-10 16:22:38 +0200

    create Xcode project

---
+++ WeatherAndStories/WeatherAndStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAndStoriesApp.swift
+//  WeatherAndStories
+//
+//  Created by Jozo Mostarac on 10.10.2024..
+//
+
+import SwiftUI
+
+@main
+struct WeatherAndStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
